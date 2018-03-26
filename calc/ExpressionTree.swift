f5cd03682a791fc0b6e4dba6b20048fc3763e7c9
Rhys Biddle <Rhys.Biddle@student.uts.edu.au>
2018-03-26 15:58:44 +1100

    add ExpressionTree struct

---
+++ calc/ExpressionTree.swift
@@ -0,0 +1,25 @@
+//
+//  ExpressionTree.swift
+//  calc
+//
+//  Created by Rhys Biddle on 26/3/18.
+//  Copyright © 2018 UTS. All rights reserved.
+//
+
+import Foundation
+
+
+
+struct ExpressionTree{
+    var root : Node
+    
+    init(postfixExpression : [String]){
+        // self.root = root
+    }
+    
+    func evaluateExpression() -> Int{
+        return 0
+    }
+    
+    
+}
